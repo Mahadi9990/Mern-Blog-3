c57bec647693f729bb97810f07daf94733404d0f
Mahadi9990 <mdshafayat9999@gmail.com>
2024-06-14 21:00:04 +0600

    cm sing-in page api route

--- api/controlars/userData.js
+++ api/controlars/userData.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const singup = async(req,res,next) => {
     const { userName, email, password } = req.body
@@ -16,3 +17,29 @@ export const singup = async(req,res,next) => {
         next(error)
     }
 }
+
+
+
+
+export const singin = async (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password || email === '' || email === '') {
+        next(errorHandler(201,'Fill up all fields'))
+    }
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) {
+            next(errorHandler(202,'User is not founds'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) {
+            next(errorHandler(203,'Wroung password'))
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JSONWEBTOKEN)
+        res.cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(validUser)
+    } catch (error) {
+       next(error) 
+    }
+ }
--- api/routes/user.outh.js
+++ api/routes/user.outh.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { singup } from "../controlars/userData.js";
+import { singup,singin } from "../controlars/userData.js";
 
 const route = express.Router()
 
 route.post('/sing-up', singup)
+route.get('/sing-in', singin)
+
 
 export default route
