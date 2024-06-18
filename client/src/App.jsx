de511cb4b12b450953c3d7318d3eac707367ef64
Mahadi9990 <mdshafayat9999@gmail.com>
2024-06-18 22:08:11 +0600

    cm private Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -17,8 +18,10 @@ export default function App() {
         <Route path='/sing-in' element={<Singin/>} />
         <Route path='/sing-up' element={<Singup/>} />
         <Route path='/profile' element={<Profile/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/about' element={<About />} />
+        <Route element={<PrivateRoute />} >
+            <Route path='/dashboard' element={<Dashboard/>} />
+        </Route>
       </Routes>
       <Footer/>
     </BrowserRouter>
