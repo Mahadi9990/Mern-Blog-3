79ba154c060e0360d606bd1d0363689df89488c5
Mahadi9990 <mdshafayat9999@gmail.com>
2024-06-19 15:06:26 +0600

    cm profile page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import {Button, TextInput} from 'flowbite-react'
 
 export default function DashProfile() {
+    const {currentUser} = useSelector((state)=>state.user)
   return (
-    <div>DashProfile</div>
+      <div className='mx-auto'>
+          <h1 className='text-center p-4 font-bold'>Profile</h1>
+          <form className='flex flex-col gap-3 md:w-[500px] w-[300px]'>
+              <div className=" w-32 h-32 self-center cursor-pointer">
+                    <img src={currentUser.avater} className='rounded-full w-full h-full border-8 border-[lightgray]' alt="" />
+              </div>
+        <TextInput
+          className='w-full'
+          type='text'
+          id='userName'
+          placeholder='User name'
+          defaultValue={currentUser.userName}
+        />
+        <TextInput
+          type='eamil'
+          id='eamil'
+          placeholder='example@gmail.com'
+          defaultValue={currentUser.email}
+        />
+        <TextInput
+          type='password'
+          id='password'
+          placeholder='password'
+        />
+        <Button gradientDuoTone='purpleToBlue' outline type='submit'>
+          Update
+        </Button>
+      </form>
+      <div className="flex justify-between pt-2">
+        <span className='text-red-600 font-semibold cursor-pointer'>Delete Account</span>
+        <span className='text-red-400 font-semibold cursor-pointer'>Sing Out</span>
+      </div>
+    </div>
   )
 }
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -16,8 +16,8 @@ export default function DashSidebar() {
       <Sidebar className='w-full md:w-56' >
           <Sidebar.Items>
               <Sidebar.ItemGroup className='md:min-h-screen'>
-                  <Link to={'/dashboard?tab=profile'}>
-                    <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'> 
+                  <Link to={'/dashboard?tab=profile'} >
+                    <Sidebar.Item as='div' active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'> 
                       Profile
                   </Sidebar.Item>
                   </Link>
